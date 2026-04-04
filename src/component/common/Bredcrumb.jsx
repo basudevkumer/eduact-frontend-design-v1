1a686b596b6cd70f9aeacce7decbc2c7c2ae0132
basudevkumar <jhulonkumar40@gmail.com>
2026-04-04 12:00:26 +0600

    feat: implement dynamic breadcrumb using useLocation

--- src/component/common/Bredcrumb.jsx
+++ src/component/common/Bredcrumb.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
-const Bredcrumb = ({ slug, slugHeading }) => {
+const Bredcrumb = () => {
+  const location = useLocation();
+
+  const slug = location.pathname.replace("/", " ");
+
   return (
     <div className="pt-[174px] pb-[154px] bg-[#000000] flex flex-col items-center">
-      <h2 className="text-[50px]/[47px] font-bold font-urbanist text-white">
-       {slugHeading}
+      <h2 className="text-[50px]/[47px] font-bold font-urbanist text-white capitalize">
+        {slug}
       </h2>
       <div className="pt-5 flex gap-x-3 items-center">
-        <span className="text-white text-xl/[47px] font-normal font-urbanist">
+        <Link to={"/"} className="text-white text-xl/[47px] font-normal font-urbanist">
           Home
-        </span>
+        </Link>
         <span className="text-white text-xl/[47px] font-normal font-urbanist">
           /
         </span>
